129c5651e2bcfc904c86d8ff50d0a9936e6542c4
Febin-Joseph <febinachu123@gmail.com>
2024-01-22 20:00:36 +0530

    added test cases in frontend

---
+++ client/Card.test.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import Card from "./src/components/card/Card"
+
+test('renders Card component with data', () => {
+  const data = {
+    id: 1,
+    company: 'being abroad',
+    location: 'kochi',
+    image: 'test-image-url',
+    course: ' Grammar Class',
+    duration: '6',
+    description: ['Description 1', 'Description 2'],
+  };
+
+  render(<Card data={data} />);
+
+  expect(screen.getByText('being abroad')).toBeInTheDocument();
+  expect(screen.getByText('kochi')).toBeInTheDocument();
+});
